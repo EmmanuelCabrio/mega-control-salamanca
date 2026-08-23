97ea4c51ea300c1166932ec3e461165aafe460d5
EmmanuelCabrio <emmanuelbriones1995@gmail.com>
2026-08-23 00:49:06 -0600

    Add PanelDireccion primer codigo

--- frontend/src/components/PanelDireccion.jsx
+++ frontend/src/components/PanelDireccion.jsx
@@ -1,1 +1,21 @@
+function PanelDireccion() {
 
+  return (
+
+    <div>
+
+      <h1>
+        👑 PANEL DE DIRECCIÓN
+      </h1>
+
+      <p>
+        Buenos días Emmanuel, este es el resumen de tu operación.
+      </p>
+
+    </div>
+
+  );
+
+}
+
+export default PanelDireccion;
